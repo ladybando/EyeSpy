471cb29f3656de5d19979e5a095a4a779cedb04e
Ayana Bando <ayanaflax@yahoo.com>
2021-05-20 16:24:04 -0400

    Update Phones.java
    add new phones/stores and add stock to stores; remove unnecessary setter/gettter

--- src/Phones.java
+++ src/Phones.java
@@ -1,118 +1,121 @@
-class Phones {
+import java.util.List;
+import java.util.Scanner;
+import java.util.*;
 
-    //phone data for each model
-        String make = "none";	// company
-        String model = "none";			// specific model
-        String os = "none";			// operating system
-        String network = "none"; //5G, 4G no data
-        int ram = 0;				// memory in GB
-        boolean sdCard = false;         //true or false
-        int cardSize= 0;          //mem card size
-        String color = "none";			// primary color
-        int lenses= 0;		// number of camera lenses
-        int resolution= 0; //MP
-        double price =  0.00;			// how much does it cost
-        int battLife = 0; //mAh
-        String security = "none"; //none, fingerprint, face
+public class EyeSpy {
 
+    public static void main(String[] args){
+        /* app should find all smartphones in inventory and search by features
+        * should know how much of each type is in stock
+       * create show method that can print all features of device
+ */     Phones[] stock = Phones.createSmart();
 
-   //constructor for basic phone/smartphone
-    public Phones(String foneMake, String foneModel, int foneResolution, int foneRam, double fonePrice, int foneBattLife, boolean foneSdCard, int foneCardSize){
-        make = foneMake;
-        model = foneModel;
-        resolution = foneResolution;
-        ram = foneRam;
-        price = fonePrice;
-        battLife = foneBattLife;
-        sdCard = foneSdCard;
-        cardSize = foneCardSize;
+        Stores newyork = new Stores();
+        newyork.setStore_Name("New York H-Eye R-Eyes");
+        newyork.setStreet_name("123 High Rise");
+        newyork.setCity_state("NY, NY");
+
+        Stores penn = new Stores();
+        penn.setStore_Name("Beast B-Eye");
+        penn.setStreet_name("789 B-eyeB-eye Street");
+        penn.setCity_state("Philadelphia, PA");
+
+        Phones ultra = new Phones();
+        ultra.setMake("Apple");
+        ultra.setModel("Pro Max 12");
+
+        Phones smart = new Phones();
+        smart.setModel("Note 12");
+        smart.setMake("Samsung");
+
+        Phones basic = new Phones();
+        basic.setMake("LG");
+        basic.setModel("Chocolate");
+
+        List<Phones> inventory = new ArrayList<>();
+        inventory.add(ultra);
+        inventory.add(smart);
+        newyork.setStore(inventory);
+
+        List<Phones> inventory1 = new ArrayList<>();
+        inventory1.add(basic);
+        penn.setStore(inventory1);
+        //System.out.println("All the stores: " + stores[0].name);
+
+        System.out.println("""
+                
+                Welcome! How can EyeSpy __---(o)_(o)---__ what you need?\s
+                Eye-ing a new phone, checking stock, or looking for a store?\s
+                """);
+        EyeSpy spy = new EyeSpy();
+
+        while(true) {
+            System.out.print("Select 'phone', 'store', 'stock', or 'q' to quit: ");
+            Scanner input = new Scanner(System.in);
+            String answer = input.next();
+            if(answer.equals("store")) {
+                System.out.print("Eye-ing all stores or stores with Androids? Enter 'all' or 'Android': ");
+                answer = input.next();
+                if (answer.equals("all")) {
+                    spy.unveilStores();
+                }else if (answer.equalsIgnoreCase("ANDROID")){
+                    System.out.println(newyork.name +" at " + newyork.street_name + ", " + newyork.city_state + " has "+ newyork.inventory.get(1) + " devices in stock.");
+                }
+            }else if (answer.equals("stock")){
+                System.out.println("EyeSpy with my little eye the total inventory: " + stock.length);
+            }
+            if(answer.equals("phone")){
+                System.out.print("Eye-ing all phones or just some features? Enter 'phone' or 'feat': ");
+                answer = input.next();
+            }
+            switch (answer) {
+                case "phone" -> spy.unveil();
+                case "feat" -> spy.unveilByFeat();
+                case "q" -> {
+                    System.out.println("Thanks for spying all you needed with us!");
+                    System.exit(0);
+                }
+            }
+            System.out.println("______---(o)_(o)---_______" + "\n");
+        }
     }
 
-    //constructor for phone/smartphone with more features
-    public Phones(String foneMake, String foneModel, String foneOs, int foneResolution, int foneRam, double fonePrice, int foneBattLife, String foneNetwork, String foneColor, boolean foneSdCard, int foneCardSize){
-        //for medium level smartphones
-        make = foneMake;
-        model = foneModel;
-        resolution = foneResolution;
-        ram = foneRam;
-        price = fonePrice;
-        battLife = foneBattLife;
-        sdCard = foneSdCard;
-        network = foneNetwork;
-        cardSize = foneCardSize;
-        color = foneColor;
-        os = foneOs;
+    //calling createSmart() method stock to iterate over stock array
+    Phones[] stock = Phones.createSmart();
+
+    public void unveil(){
+        //will show all phones for each phone in phones
+        for (Phones phones : stock) {
+            System.out.println(phones.make + " " + phones.model +
+                    "\n  Software: " + phones.os +
+                    "\n  Photo Resolution -  megapixels: " + phones.resolution +
+                    "\n  Memory - GB: " + phones.ram +
+                    "\n  Price: " + phones.price +
+                    "\n  Battery Life - mAh: " + phones.battLife +
+                    "\n  Network: " +  phones.network +
+                    "\n  Memory Card Slot: " + phones.sdCard +
+                    "\n  Memory Card Size - GB: " + phones.cardSize +"\n");
+
+        }
     }
 
-    //constructor for smartphone with advanced features
-    public Phones(String foneMake, String foneModel, String foneOs, int foneResolution, int foneRam, double fonePrice,
-                  int foneBattLife, String foneNetwork, String foneColor, int foneLens, String foneSec, boolean foneSdCard, int foneCardSize){
-        //;ultra premium phone with all features
-        make = foneMake;
-        model = foneModel;
-        resolution = foneResolution;
-        ram = foneRam;
-        price = fonePrice;
-        battLife = foneBattLife;
-        network = foneNetwork;
-        color = foneColor;
-        lenses = foneLens;
-        security = foneSec;
-        sdCard = foneSdCard;
-        cardSize = foneCardSize;
-        os = foneOs;
-    }
-    //getters
-    public static String getMake(String make) { return make;}
 
-    public static String getModel(String model) { return model;}
+    public void unveilByFeat() {
+        //searches for phones in stock by os-Android
 
-    public static String getOs(String os) {return os;}
-
-    public static int getRam(int ram) {return ram;}
-
-    public static int getLenses(int camera_lenses){
-        return camera_lenses;
+        for (Phones phone : stock)
+         if (phone.os.equals("Android")){
+             System.out.println("EyeSpy with my little eye all Android Phones: \n" + phone.make + phone.model);
+        }
     }
 
-    public static double getPrice( double price){
-        return price;
+
+    public void unveilStores(){
+        //prints all stores
+        Stores[] stores = Stores.createStore();
+        System.out.println("EyeSpy with my little eye all stores:");
+        for (Stores store : stores) {
+            System.out.println(store.name + ":\n" + store.street_name + "\n" + store.city_state + "\n" + store.phone_number + "\n\n");
+        }
     }
-
-    public static boolean getSdCard(boolean sdCard){ return sdCard;}
-
-    public static int getCardSize(int cardSize){return cardSize;}
-
-    public static String getColor(String color){return color;}
-
-    public static int getResolution(int resolution){return resolution;}
-
-    public static int getBattLife(int battLife){return battLife;}
-
-    public static String getSecurity(String security){return security;}
-    //setters
-
-    public void setMake(String make) { this.make = make;}
-
-    public void setModel(String model) { this.model = model;}
-
-    public void setOs(String os) {this.os = os;}
-
-    public void setRam(int ram) {this.ram = ram;}
-
-    public void setLenses(int lenses){ this.lenses = lenses; }
-
-    public void setPrice( double price){ this.price = price;}
-
-    public void setSdCard(boolean sdCard){ this.sdCard = sdCard;}
-
-    public void setCardSize(int cardSize){this.cardSize = cardSize;}
-
-    public void setColor(String color){this.color = color;}
-
-    public void setResolution(int resolution){this.resolution = resolution;}
-
-    public void setBattLife(int battLife){this.battLife = battLife;}
-
-    public void setSecurity(String security){this.security = security;}
 }
