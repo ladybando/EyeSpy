c4d771fbefef7ac8bbae99ea62fd0b6497458da3
Ayana Bando <ayanaflax@yahoo.com>
2021-05-20 16:25:46 -0400

    Update Stores.java
    update setters/getters and add new no param constructor

--- src/Stores.java
+++ src/Stores.java
@@ -1,9 +1,30 @@
+import java.util.List;
+
 public class Stores {
-    String name;			// name of the store
+    String name;
     String street_name;			// street address of the store
     String city_state;
     String phone_number;
+    List<Phones> inventory;
 
+    public void setStore_Name(String name) {
+        this.name = name;
+    }
+
+    public void setStreet_name(String street_name) {
+        this.street_name = street_name;
+    }
+
+    public void setCity_state(String city_state) {
+        this.city_state = city_state;
+    }
+
+    public void setStore(List<Phones> inventory){
+        this.inventory = inventory;
+    }
+
+
+    public Stores(){}
 
     public Stores(String store_name, String store_street_name, String store_city_state, String store_phone_number) {
         name = store_name;
@@ -12,28 +33,17 @@ public class Stores {
         phone_number = store_phone_number;
     }
 
-    public static String getName(String name){return name;}
-    public static String getStreet( String street_name){return street_name;}
-    public static String getCity( String city_state){return city_state;}
-    public static int getNumber(int phone_number){return phone_number;}
-    public void setStore(String name, String street_name, String city_state, String phone_number){
-        this.name = name;
-        this.street_name = street_name;
-        this.city_state = city_state;
-        this.phone_number = phone_number;
-    }
-
     public static Stores[] createStore(){
         Stores[] stores;
-        Stores newyork = new Stores("New York H-Eye R-Eyes", "123 High Rise", "NY, NY", "(917)444-7443");
         Stores newjersey = new Stores("NJ Long Beach Eye-Land", "456 LBEye Street", "Long Beach, NJ", "(609)393-5263");
         Stores penn = new Stores("Beast B-Eye", "789 B-eyeB-eye Street", "Philadelphia, PA", "(215)232-7824");
-        for (Stores store : stores = new Stores[]{newyork,newjersey,penn}) {
+        for (Stores store : stores = new Stores[]{newjersey,penn}) {
             return new Stores[]{store, newjersey, penn};
         }
         return stores;
     }
 
+}
     public static String addPhoneToStore(){
        // Phones[] stock = Inventory.createSmart(); 	// inventory of Smartphones in this store
         return "";
